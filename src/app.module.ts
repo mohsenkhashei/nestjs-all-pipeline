d377ed494cf249aa842a51b497db845a907e5d2f
MohsenKhashei <m.khasheii@gmail.com>
2023-07-25 17:49:29 +0300

    adding guard with 3 ways of adding to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RequestService } from './request.service';
 import { AuthenticationMiddleware } from './middleware/authentication.middleware';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './guards/auth.guard';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, RequestService],
+  providers: [
+    AppService,
+    RequestService,
+    { provide: APP_GUARD, useClass: AuthGuard }, // third way of adding guard
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
